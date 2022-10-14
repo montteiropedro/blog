53f8bccc0d9d7a310c3fc4f61be3d7d17c14533d
Pedro Monteiro <montteiropedro@gmail.com>
2022-10-14 12:48:08 -0300

    updates in Articles controller and Articles show view, add new route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "articles#index"
   
   get "/articles", to: "articles#index"
-
+  get "/articles/:id", to: "articles#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end
